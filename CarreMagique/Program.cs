33933088af600b4deffa447d3a2ba821c284af1d
arodde <roddealain@gmail.comgit config --global user.email roddealain@gmail.comgit config --global user.email roddealain@gmail.com>
2022-01-11 10:48:26 +0100

    constat problème: test avant modification

--- CarreMagique/Program.cs
+++ CarreMagique/Program.cs
@@ -5,13 +5,21 @@ namespace CarreMagique
 {
     class Program
     {
+        static void Main2(string[] args)
+        {
+            Test1 test1 = new Test1();
+
+            test1.lancer();
+        }
         static void Main(string[] args)
         {
             Uti.Info("Program", "Main", "");
-            Uti.ReferencesProgramme("RODDE","Alain","carre-magique","1.0.2");
+            Uti.ReferencesProgramme("RODDE", "Alain", "carre-magique", "1.0.2");
             Menu menu = new Menu();
             menu.MethodesMenuJeu();
             Console.WriteLine();
+
+
         }
     }  
 
